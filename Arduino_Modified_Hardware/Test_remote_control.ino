a65e362a4e4452a9feb0eccbb1a43a88a3439094
rtek1000 <rh.silicon@gmail.com>
2022-07-17 16:27:02 -0300

    Update Test_remote_control.ino

--- Arduino_Modified_Hardware/Test_remote_control.ino
+++ Arduino_Modified_Hardware/Test_remote_control.ino
@@ -17,6 +17,13 @@
 
     Other modifications:
     - 2022-07-17: Added interruption for keyboard
+
+    - 2022-07-17: Relocated RF signal input from remote control, from PB4 to PD4.
+    Note: The RF signal input from the remote control is originally connected to the PB4 pin,
+          but on this pin there is the I2C hardware module. To expand the capacity of available pins,
+          without needing much hardware change, I chose to use a PCF8574 port expander.
+          For this I modified (cut) the track of the RF receiver of the remote control and
+          connected it to the PD4 pin of the uC STM8.
 */
 
 #include"stm8s.h"
